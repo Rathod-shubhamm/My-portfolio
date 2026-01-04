a8d90a91212f0e5bb89e930991a3cf97d789c13d
Apex-Shubham <shubham.rathod@apexneural.com>
2026-01-04 23:51:20 +0530

    Menu section image updation

--- src/components/header/config.ts
+++ src/components/header/config.ts
@@ -4,12 +4,12 @@ const links: Link[] = [
   {
     title: 'Home',
     href: '/',
-    thumbnail: '/assets/nav-link-previews/landing.png'
+    thumbnail: '/assets/nav-link-previews/landingpagess.png'
   },
   {
     title: 'About',
     href: '/#about',
-    thumbnail: '/assets/nav-link-previews/about.png'
+    thumbnail: '/assets/nav-link-previews/landingpagess.png'
   },
   {
     title: 'Skills',
@@ -19,7 +19,7 @@ const links: Link[] = [
   {
     title: 'Projects',
     href: '/#projects',
-    thumbnail: '/assets/nav-link-previews/projects.png'
+    thumbnail: '/assets/nav-link-previews/projectss.png'
   },
   // {
   //   title: 'Skills',
@@ -31,11 +31,7 @@ const links: Link[] = [
   //   href: '/testimonials',
   //   thumbnail: '/assets/nav-link-previews/testimonials.png'
   // },
-  {
-    title: 'Blogs',
-    href: '/blogs',
-    thumbnail: '/assets/nav-link-previews/blog.png',
-  },
+
   {
     title: 'Contact',
     href: '/#contact',
